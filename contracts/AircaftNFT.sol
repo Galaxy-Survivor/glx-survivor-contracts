5bb78f22082c8ff3b7cce13f34cacc7b6c393e70
Nguyen Van Hiep <nvhbk16k53@gmail.com>
2021-09-15 17:46:42 +0700

    Add function for mint lucky box and mint batch of components

--- contracts/AircaftNFT.sol
+++ contracts/AircaftNFT.sol
@@ -5,10 +5,20 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
 contract AircraftNFT is ERC1155PresetMinterPauser {
-	uint256 maxComponentId = 1000;
-	uint256 currentId = 1000;
+	uint256 LUCKY_BOX_ID = 0;
+	uint256 MAX_COMPONENT_ID = 10000;
+
+	uint256 currentId = 10000;
 
 	constructor(string memory _uri) ERC1155PresetMinterPauser(_uri) {}
+
+	modifier onlyMinter() {
+		require(
+			hasRole(MINTER_ROLE, _msgSender()),
+			"AircraftNFT: must have minter role to mint"
+		);
+		_;
+	}
 
 	function addMinter(address _minter) external {
 		require(
@@ -19,12 +29,7 @@ contract AircraftNFT is ERC1155PresetMinterPauser {
 		grantRole(MINTER_ROLE, _minter);
 	}
 
-	function mintAircraft(address _to, uint256 _amount) external {
-		require(
-			hasRole(MINTER_ROLE, _msgSender()),
-			"AircraftNFT: must have minter role to mint"
-		);
-
+	function mintAircraft(address _to, uint256 _amount) external onlyMinter {
 		uint256 startId = currentId;
 		currentId += _amount;
 
@@ -33,17 +38,33 @@ contract AircraftNFT is ERC1155PresetMinterPauser {
 		}
 	}
 
-	function mintComponent(address _to, uint256 _id, uint256 _amount) external {
-		require(
-			hasRole(MINTER_ROLE, _msgSender()),
-			"AircraftNFT: must have minter role to mint"
-		);
+	function mintLuckyBox(address _to, uint256 _amount) external onlyMinter {
+		_mint(_to, LUCKY_BOX_ID, _amount, "");
+	}
 
+	function mintComponent(address _to, uint256 _id, uint256 _amount) external onlyMinter {
 		require(
-			_id < maxComponentId,
+			_id <= MAX_COMPONENT_ID && _id > LUCKY_BOX_ID,
 			"AircraftNFT: id must less than or equal to max component id"
 		);
 
 		_mint(_to, _id, _amount, "");
+	}
+
+	function mintComponents(
+		address _to,
+		uint256[] memory _ids,
+		uint256[] memory _amounts
+	) external {
+		bool isValid = true;
+		for (uint256 i = 0; i < _ids.length; i++) {
+			if (_ids[i] > MAX_COMPONENT_ID || _ids[i] == LUCKY_BOX_ID) {
+				isValid = false;
+				break;
+			}
+		}
+		require(isValid, "AircraftNFT: id must less than or equal to max component id");
+
+		_mintBatch(_to, _ids, _amounts, "");
 	}
 }
