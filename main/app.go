9077c7ce6c1c2f94c5ca007ff84fd1b34f3c147c
DuongTx <duongtx90@gmail.com>
2021-10-04 08:57:09 +0700

    add read event

--- main/app.go
+++ main/app.go
@@ -6,38 +6,24 @@ import (
 	"log"
 	"math/big"
 	"os"
-	"strings"
 
 	NFT "app/nft"
 
 	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
-<<<<<<< HEAD
 	args := os.Args[1:]
 	client, err := ethclient.Dial(args[0])
-=======
-
-	client, err := ethclient.Dial("ws://192.168.1.199:8545")
->>>>>>> c55865f24445e5512355bf39ec90f3fc2cd6dc50
 	if err != nil {
 		log.Fatal(err)
 	}
 
-<<<<<<< HEAD
 	contractAddress := common.HexToAddress(args[1])
 	query := ethereum.FilterQuery{
-		FromBlock: big.NewInt(3),
-=======
-	contractAddress := common.HexToAddress("0xe31267c0eedB0F23DB3C62E8eBa3dFf13e18345b")
-	query := ethereum.FilterQuery{
-		FromBlock: big.NewInt(0),
->>>>>>> c55865f24445e5512355bf39ec90f3fc2cd6dc50
+		FromBlock: big.NewInt(1),
 		ToBlock:   big.NewInt(2394201),
 		Addresses: []common.Address{
 			contractAddress,
@@ -49,11 +35,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	contractAbi, err := abi.JSON(strings.NewReader(string(NFT.AircraftNFTABI)))
-	if err != nil {
-		log.Fatal("ERROR PARSE:", err)
-	}
-	fmt.Println("====================")
+	// contractAbi, err := abi.JSON(strings.NewReader(string(NFT.AircraftNFTABI)))
+
+	aircraftNFT, err := NFT.NewAircraftNFT(contractAddress, client)
+
+	fmt.Println(len(logs))
 	for _, vLog := range logs {
 		fmt.Println("\n\n********************************")
 		fmt.Println("BlockHash", vLog.BlockHash.Hex()) // 0x3404b8c050aa0aacd0223e91b5c32fee6400f357764771d0684fa7b3f448f1a8
@@ -61,22 +47,21 @@ func main() {
 		fmt.Println("TxHash", vLog.TxHash.Hex())       // 0x280201eda63c9ff6f305fcee51d5eb86167fab40ca3108ec784e8652a0e2b1a6
 		fmt.Println("Data", vLog.Data)                 // 0x280201eda63c9ff6f305fcee51d5eb86167fab40ca3108ec784e8652a0e2b1a6
 
-		var eventRet struct {
-			address common.Address
-			id      *big.Int
-		}
+		// var event struct {
+		// 	address common.Address
+		// 	id      *big.Int
+		// }
 
-		err := contractAbi.UnpackIntoInterface(&eventRet, "event_MintedAircraft", vLog.Data)
+		event, err := aircraftNFT.AircraftNFTFilterer.ParseEventMintedAircraft(vLog)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error", err)
 			continue
 		}
 		// for i := range interfaces {
-		// 	fmt.Println((interfaces[i])) // bar
+		//      fmt.Println((interfaces[i])) // bar
 		// }
-		fmt.Printf("Address %v - ID %v\n", eventRet.address.Hash(), eventRet.id) // foo
+		fmt.Printf("Address %v - ID %v\n", event.Operator.Hash(), event.AircraftId) // foo
 		// fmt.Println(string(inter.Value[:])) // bar
-
 		var topics [4]string
 		for i := range vLog.Topics {
 			topics[i] = vLog.Topics[i].Hex()
@@ -84,8 +69,5 @@ func main() {
 		fmt.Println("TOPIC: ", topics[0])
 		fmt.Println("====================")
 	}
-
-	eventSignature := []byte("event_MintedAircraft(address,uint256)")
-	hash := crypto.Keccak256Hash(eventSignature)
-	fmt.Println("eventSignature: ", hash.Hex()) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+	// NFT.WatchEventMintedAircraft(client)
 }
