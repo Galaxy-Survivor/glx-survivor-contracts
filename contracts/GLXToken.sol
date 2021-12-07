8e747f57a17f5ce712a5452bfec50b23bf944f6b
Nguyen Van Hiep <nvhbk16k53@gmail.com>
2021-12-07 13:37:39 +0000

    Implement contract for GLX token

---
+++ contracts/GLXToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract GLXToken is ERC20Capped, AccessControl {
+	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+	constructor()
+		ERC20("Galaxy Token", "GLX")
+		ERC20Capped(1e9 * 1e18)
+	{
+		_setupRole(MINTER_ROLE, msg.sender);
+	}
+
+	modifier onlyMinter(address account) {
+		require(hasRole(MINTER_ROLE, account), "GLXToken: require minter role");
+		_;
+	}
+
+	function mint(address _to, uint256 _amount)
+		external
+		onlyMinter(msg.sender)
+	{
+		super._mint(_to, _amount);
+	}
+}
